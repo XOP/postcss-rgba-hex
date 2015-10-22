c08ca3d5eede762229cec822e8c86b15873bc748
XOP <wdybih@gmail.com>
2015-10-22 13:38:34 +0300

    v.0.2.0 basic transformation

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
+/**
+ * Module RGB(a)-HEX
+ * @type {"postcss"}
+ */
+
+//
+// Module dependencies
+
 var postcss = require('postcss');
 var rgb2hex = require('rgb2hex');
 
-module.exports = function(opts) {
-    var inputString = 'rgba(255,255,255,.5)';
-    var outputHex = rgb2hex(inputString).hex;
+//
+// RGB(a) regex
+var rgbaReg = /rgba?\(\d+%?\s*,\s*\d+%?\s*,\s*\d+%?\s*(,\s*\d?.?\d+)?\)/g;
 
-    return outputHex;
-};
+/**
+ * PostCSS plugin
+ * @type {*}
+ */
+module.exports = postcss.plugin('postcss-rgba-hex', function(options) {
 
-//module.exports = postcss.plugin('postcss-rgba-hex', function(opts) {
-//    // ...
-//});
+    return function(style) {
+        style.walkDecls(function(decl) {
+            var val = decl.value;
+
+            // early return
+            if(!val) {
+                return;
+            }
+
+            // stripping values
+            var rgbValues = val.match(rgbaReg);
+
+            // converting values
+            if(rgbValues && rgbValues.length > 0){
+                var newVal = val;
+                rgbValues.forEach(function(rgb) {
+                    newVal = newVal.replace(rgb, rgbaToHex(rgb));
+                    console.info('RGB(a) replaced: ' + rgb + ' -> ' + rgbaToHex(rgb));
+                });
+                decl.value = newVal;
+            }
+        })
+    };
+});
+
+/**
+ * RGBA(a) to hex transformer
+ * @param rgbaString
+ * @returns {string}
+ */
+function rgbaToHex(rgbaString) {
+    var hexString = '';
+    hexString = rgb2hex(rgbaString).hex;
+
+    return hexString;
+}
--- test.js
+++ test.js
@@ -1,4 +1,12 @@
-var test = require('./index.js');
-var hex = test();
+var fs = require('fs');
 
-console.log(hex);
+var rgbaToHex = require('./index.js');
+var postcss = require('postcss');
+
+var css = fs.readFileSync('./tests/test.css');
+
+postcss([ rgbaToHex ])
+    .process(css)
+    .then(function(result) {
+        console.log('Result CSS: ' + result.css);
+    });
